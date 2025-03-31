4a9e3743d279ead0d43c6d15b360548ed4245aca
Srutanik Bhaduri <srutanik.bhaduri02@gmail.com>
2025-03-31 17:17:27 +0530

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Set base to './' for GitHub Pages deployment
+  base: '/bdsl-website',  // Set base to './' for GitHub Pages deployment
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
