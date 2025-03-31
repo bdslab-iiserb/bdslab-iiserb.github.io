39ee5185f848d50cf4f6b0b3723495b43f550425
Srutanik Bhaduri <srutanik.bhaduri02@gmail.com>
2025-04-01 05:25:14 +0530

    Revert "Removed Blog"
    This reverts commit b98c37651ba2bd35425af554e27492c5264c534b.
    
    git commit -m "Revert back"

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Team from './pages/Team';
 import Research from './pages/Research';
-//import Blog from './pages/Blog';
+import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 
 export default function App() {
@@ -16,7 +16,7 @@ export default function App() {
             <Route path="/" element={<Home />} />
             <Route path="/team" element={<Team />} />
             <Route path="/research" element={<Research />} />
-{/*            <Route path="/blog" element={<Blog />} /> */}
+            <Route path="/blog" element={<Blog />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
